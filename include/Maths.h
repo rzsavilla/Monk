dc09c97ef246370a527d9cad1a1611d74abebeec
Zoa84 <zoa84death@hotmail.co.uk>
2016-01-30 11:06:07 +0000

    Added Maths.h

---
+++ include/Maths.h
@@ -0,0 +1,18 @@
+#ifndef MATHS_H
+#define MATHS_H
+
+#include <SFML/Graphics.hpp>
+
+namespace Maths
+{
+	const float PI = 3.14159;
+	const float RadToDeg = 180 / PI;
+	const float DegToRad = PI / 180;
+
+	float GetMag(sf::Vector2f vector)
+	{
+		return sqrt((vector.x * vector.x) + (vector.y * vector.y));
+	}
+};
+
+#endif
